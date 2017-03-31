d4e9935af00e8986c4c0764a77b82f55074a7560
Kent Johnson <kent3737@gmail.com>
2017-03-31 14:28:24 -0400

    Initial commit
    Map representatives by district or committee

---
+++ Mapper.R
@@ -0,0 +1,47 @@
+# Make maps
+
+library(htmltools)
+library(leaflet)
+library(sp)
+source('Readers.R')
+
+# Map by district
+to_map = read_reps() %>% by_district(districts)
+
+# Map by committee
+to_map = read_reps() %>% by_committee('HLIG')
+
+map_reps = function(to_map) {
+  fips = read_fips()
+   to_map = to_map %>% 
+    left_join(fips, by=c(state='USPS'))
+  
+  boundaries = raster::shapefile('data/tl_2016_us_cd115/tl_2016_us_cd115.shp')
+  bounds = merge(boundaries, to_map, 
+                     by.x=c('STATEFP', 'CD115FP'), by.y=c('FIPS', 'district'),
+                     all.x=FALSE) %>% 
+    spTransform(CRS("+init=EPSG:4326"))
+  
+  make_popup = function(d) {
+    lapply(1:nrow(d),
+       function(i) {
+         row = d[i,]
+   HTML(as.character(p(
+     tags$a(tags$b(row$official_full), href=row$url, target='_blank'), br(),
+     row$code, br(),
+     row$phone
+     )))
+   })
+  }
+  
+  bounds$popup = make_popup(bounds@data)
+  bounds$color = ifelse(bounds$party=='Democrat', 'blue', 
+                        ifelse(bounds$party=='Republican', 'red', 'darkyellow'))
+  
+  leaflet(bounds) %>% addProviderTiles('Esri.WorldTopoMap') %>% 
+    addPolygons(color='black', fillColor=~color, weight=2, fillOpacity=0.4,
+                label=~paste(official_full, code, sep=', '), popup=~popup,
+                highlightOptions = 
+                highlightOptions(opacity=1, fillOpacity=0.6,
+                                 weight=3, bringToFront = TRUE))
+}
---
+++ Readers.R
@@ -0,0 +1,47 @@
+# Helpers
+library(lubridate)
+library(stringr)
+library(tidyverse)
+
+#' Read state FIPS codes and return a tibble.
+read_fips = function() {
+  read_csv("data/State_FIPS.csv",
+    col_types = cols(FIPS = col_character()))
+}
+
+#' Read data about representatives.
+#' Expects https://github.com/unitedstates/congress-legislators
+#' to be in a peer directory
+read_reps = function() {
+  reps = yaml::yaml.load_file('../congress-legislators/legislators-current.yaml') %>%
+    map_df(~bind_cols(.$name, tibble(bioguide=.$id$bioguide), .$bio, .$terms[[length(.$terms)]])) %>% 
+    filter(type=='rep', ymd(end) > Sys.Date()) %>% # Filter only current reps
+    mutate(district=str_pad(district, 2, pad='0'), # Pad district to two chars
+           code=paste0(str_sub(party, 1, 1), '-',  # Code like D-MA-07
+                       state, '-', district))
+  reps
+}
+
+#' Read committees. Skip sub-committes for now
+read_committees = function() {
+  comms = yaml::yaml.load_file('../congress-legislators/committees-current.yaml') %>%     map_df(~as_tibble(.[setdiff(names(.), 'subcommittees')]))
+  comms
+}
+
+#' Find all reps on a committee
+#' @param comm Thomas code for the committee, e.g. 'HSAG'
+by_committee = function(reps, comm) {
+  all_assignments = yaml::yaml.load_file('../congress-legislators/committee-membership-current.yaml')
+  this_comm = all_assignments[[comm]] %>% map_df(~as_tibble(.)) %>% 
+    select(bioguide, title)
+  reps %>% right_join(this_comm, by='bioguide')
+}
+
+#' Get just the reps in the given districts.
+#' @param districts Vector of districts in the form MA-05
+by_district = function(reps, districts) {
+  str_split_fixed(districts, '-', 2) %>% 
+    as_tibble %>% 
+    set_names(c('state', 'district')) %>% 
+    left_join(reps)
+}
