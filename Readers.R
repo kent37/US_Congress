a9e198761cab6c7ad71af96c73150e75a0f6b23d
Kent Johnson <kent3737@gmail.com>
2017-04-02 19:52:41 -0400

    Make house committee cross-ref

--- Readers.R
+++ Readers.R
@@ -37,6 +37,16 @@ by_committee = function(reps, comm) {
   reps %>% right_join(this_comm, by='bioguide')
 }
 
+#' Make a cross-reference of committee ID and name to House member bioguide
+house_committee_xref = function() {
+  comms = read_committees() %>% filter(type!='senate') %>% select(name, thomas_id)
+  assignments = yaml::yaml.load_file('../congress-legislators/committee-membership-current.yaml')[names(assignments) %in% comms$thomas_id]
+  tibble(thomas_id=names(assignments), 
+                        members=map(assignments, ~map_df(., as_tibble))) %>% 
+    unnest %>% select(thomas_id, bioguide) %>% 
+    inner_join(comms)
+}
+
 #' Get just the reps in the given districts.
 #' @param districts Vector of districts in the form MA-05
 by_district = function(reps, districts) {
