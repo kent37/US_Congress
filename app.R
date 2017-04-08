8974f3074351977132f39022b19d005c575c3c33
Kent Johnson <kent3737@gmail.com>
2017-04-08 09:07:50 -0400

    Render map differently depending on zoom level
    Thick borders at low zoom, more transparent at high zoom. Some formatting tweaks.

--- app.R
+++ app.R
@@ -9,8 +9,8 @@ if (file.exists('data/bounds.RData')) {
   load('data/bounds.RData')
 } else {
   bounds = make_boundaries(reps)
-
 }
+
 bounds$label = paste(bounds$official_full, bounds$code, sep=', ')
 comm_xref = house_committee_xref()
 
@@ -28,7 +28,19 @@ server <- function(input, output) {
                 highlightOptions = 
                 highlightOptions(opacity=1, fillOpacity=0.6,
                                  weight=3, bringToFront = TRUE))
-
+  })
+  
+  output$zoom = renderText(input$map_zoom)
+  
+  # Zoom levels
+  level = reactiveVal(1)
+  
+  # Watch for zoom changes. Only propagate them when level changes.
+  observe({
+      level_ = if (is.null(input$map_zoom) || input$map_zoom <= 5) 1
+      else if (input$map_zoom <= 7) 2
+      else 3
+    if (level() != level_) level(level_)
   })
   
   observe({
@@ -36,30 +48,63 @@ server <- function(input, output) {
     selected_bounds = bounds
     if (nrow(selected_comms) > 0)
       selected_bounds = selected_bounds[bounds$bioguide %in% selected_comms$bioguide,]
-    leafletProxy('map', data=selected_bounds) %>% 
+    
+    extra_highlight = level() == 1
+    if (level() == 1) {
+      weight = 4
+      opacity = 1
+      fillOpacity = 0.6
+      highlightWeight = 5
+      highlightFillOpacity = 0.6
+    } else {
+      weight = 2
+      opacity = 0.5
+      highlightWeight = 3
+      fillOpacity = if (level() == 3) 0.1 else 0.4
+      highlightFillOpacity = if (level()==3) 0.1 else 0.6
+    }
+    
+    # Add Democrats last, their districts tend to be small
+    leafletProxy('map') %>% 
       clearShapes() %>% 
-    addPolygons(color='black', 
-                fillColor=~color, 
-                weight=2, fillOpacity=0.4,
+    addPolygons(data=selected_bounds[selected_bounds$party!='Democrat',],
+                color=~color, 
+                fillColor=~color,  opacity=opacity,
+                weight=weight, fillOpacity=fillOpacity,
                 label=~label, 
                 popup=~popup,
                 highlightOptions = 
-                highlightOptions(opacity=1, fillOpacity=0.6,
-                                 weight=3, bringToFront = TRUE))
+                highlightOptions(opacity=1, fillOpacity=highlightFillOpacity,
+                                 weight=highlightWeight, bringToFront = TRUE)) %>% 
+    addPolygons(data=selected_bounds[selected_bounds$party=='Democrat',],
+                color=~(if (extra_highlight) color else 'black'), 
+                fillColor=~color,  opacity=opacity,
+                weight=weight, fillOpacity=fillOpacity,
+                label=~label, 
+                popup=~popup,
+                highlightOptions = 
+                highlightOptions(opacity=1, fillOpacity=highlightFillOpacity,
+                                 weight=highlightWeight, bringToFront = TRUE))
 
   })
 }
 
-ui <- fluidPage(title = "Committees of the US House of Representatives",
-  titlePanel("Committees of the US House of Representatives"),
+ui <- navbarPage(title = "Committees of the US House of Representatives",
+                 inverse=TRUE,
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", 
+href = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/spacelab/bootstrap.min.css")),
+  tabPanel(""),
   fluidRow(
     column(6, offset=1,
-    selectizeInput('comms', 'Committee', comm_xref$name, multiple = FALSE,
-               options = NULL, width='400px')
-  )),
+    selectizeInput('comms', 'Choose a committee:', 
+               unique(comm_xref$name), multiple = FALSE,
+               options = NULL, width='400px',
+               selected="House Permanent Select Committee on Intelligence"))
+  ),
   fluidRow(
       leafletOutput('map', width='95%', height=600)
-)
+  )
 )
 
 
